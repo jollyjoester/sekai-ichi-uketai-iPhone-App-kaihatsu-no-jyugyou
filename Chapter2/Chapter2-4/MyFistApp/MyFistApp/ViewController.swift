6e7f8764f0ef7ee6221a0b8e64f3d1cc5dd1a3c2
jollyjoester <jollyjoester@gmail.com>
2017-01-04 02:33:45 +0900

    Change chapter2 to swift3

--- Chapter2/Chapter2-4/MyFistApp/MyFistApp/ViewController.swift
+++ Chapter2/Chapter2-4/MyFistApp/MyFistApp/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func tappedButton(sender: AnyObject) {
+    @IBAction func tappedButton(_ sender: AnyObject) {
         label.text = "めでたい!!"
     }
 }
